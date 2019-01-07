f403e22329f5deb38a8ad8f5719a35c07c6ed8e3
Mariel Waters <marielcwaters@gmail.com>
2019-01-06 19:34:15 -0700

    Add photo class

---
+++ photo.js
@@ -0,0 +1,28 @@
+class Photo {
+  constructor(id, title, caption, file, favorite){
+		this.id = id;
+		this.title = title;
+		this.caption = caption;
+		this.file = file;
+    this.favorite = favorite;
+  }
+  saveToStorage() {
+
+  }
+  deleteFromStorage() {
+
+  }
+
+  updateTitle() {
+
+  }
+
+  updatePhoto() {
+
+  }
+
+  updateCaption() {
+
+  }
+
+}
